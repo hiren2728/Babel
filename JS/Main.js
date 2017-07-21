7d3b713d237788c833686a907530c04d2e2c513f
Hiren Gajra <ios5.webmobtech@gmail.com>
2017-07-21 11:07:14 +0530

    - first initial setup

---
+++ JS/Main.js
@@ -0,0 +1,3 @@
+import date from "./second.js"
+
+alert(date.myDateTime())
---
+++ build/build.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var _second = require("./second.js");
+
+var _second2 = _interopRequireDefault(_second);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+alert(_second2.default.myDateTime());
